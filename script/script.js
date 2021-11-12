2cff2c2448458c459dd2e659a62c0986cae87a9b
Shaktyka <k-i-s-u-n-j-a@list.ru>
2021-11-12 07:43:14 +0500

    Выполняет код дня 1

---
+++ script/script.js
@@ -0,0 +1,51 @@
+'use strict';
+
+document.addEventListener('DOMContentLoaded', () => {
+    const customerBtn = document.getElementById('customer');
+    const freelancerBtn = document.getElementById('freelancer');
+    const blockCustomer = document.getElementById('block-customer');
+    const blockFreelancer = document.getElementById('block-freelancer');
+    const blockChoice = document.getElementById('block-choice');
+    const btnExit = document.getElementById('btn-exit');
+    const formCustomer = document.getElementById('form-customer');
+
+    const orders = [];
+
+    customerBtn.addEventListener('click', () => {
+        // console.log(1);
+        blockChoice.style.display = 'none';
+        blockCustomer.style.display = 'block';
+        btnExit.style.display = 'block';
+    });
+
+    freelancerBtn.addEventListener('click', () => {
+        // console.log(2);
+        blockChoice.style.display = 'none';
+        blockFreelancer.style.display = 'block';
+        btnExit.style.display = 'block';
+    });
+
+    btnExit.addEventListener('click', () => {
+        blockCustomer.style.display = 'none';
+        blockFreelancer.style.display = 'none';
+        blockChoice.style.display = 'block';
+        btnExit.style.display = 'none';
+    });
+
+    formCustomer.addEventListener('submit', (evt) => {
+        evt.preventDefault();
+        const elemObj = {};
+
+        for (const elem of formCustomer.elements) {
+            if ((elem.tagName === 'INPUT' && elem.type !== 'radio') ||
+                (elem.type === 'radio' && elem.checked) ||
+                (elem.tagName === 'TEXTAREA')) {
+                elemObj[elem.name] = elem.value;
+            }
+        }
+
+        orders.push(elemObj);
+
+        formCustomer.reset();
+    });
+});
